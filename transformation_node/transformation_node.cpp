22c89044d7cd557d4dd31c43722977f832a7201d
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-02-16 19:37:07 +0400

    changing to camera model

---
+++ transformation_node/transformation_node.cpp
@@ -0,0 +1,51 @@
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include "geometry_msgs/Point.h"
+
+
+double pitch;
+double roll;
+
+
+void yawCallback(const geometry_msgs::Point& msg){
+  static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = "inertial";
+  transformStamped.child_frame_id = "body";
+  tf2::Quaternion q;
+  q.setRPY(roll, pitch, msg.x);
+  transformStamped.transform.rotation.x = q.x();
+  transformStamped.transform.rotation.y = q.y();
+  transformStamped.transform.rotation.z = q.z();
+  transformStamped.transform.rotation.w = q.w();
+
+  br.sendTransform(transformStamped);
+}
+
+void rollCallback(const geometry_msgs::Point& msg){
+
+    roll=msg.x;
+
+}
+
+void pitchCallback(const geometry_msgs::Point& msg){
+
+    pitch=msg.x;
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "my_tf2_broadcaster");
+
+   
+  ros::NodeHandle node;
+  ros::Subscriber sub_roll = node.subscribe("/providers/roll", 10, &rollCallback);
+  ros::Subscriber sub_pitch = node.subscribe("/providers/pitch", 10, &pitchCallback);
+  ros::Subscriber sub_yaw = node.subscribe("/providers/yaw", 10, &yawCallback);
+
+  ros::spin();
+  return 0;
+};
